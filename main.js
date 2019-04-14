d35e521a20d1994f761041c47ef2b898d7085e6e
zhaojintian <729234283@qq.com>
2019-04-15 00:15:33 +0800

    优化

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from '@/app'
-import store from '@/store'
-import globalPlugin from '@/plugins/global'
-import '@/utils/init'
+import App from './App'
+import store from './store'
+import globalPlugin from './plugins/global'
+import './utils/init'
 
 Vue.config.productionTip = false
 
