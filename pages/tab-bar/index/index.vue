c9fe68a49c55728d70574d1083ee7b929d1a743e
zhaojintian <729234283@qq.com>
2019-04-15 20:55:40 +0800

    优化

---
+++ pages/tab-bar/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-index">
+    abc
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles.scss">
+</style>
--- utils/consts.js
+++ utils/consts.js
@@ -1,5 +1,5 @@
 // 接口地址
-const API_URL = 'http://localhost:3002/api/v1'
+const API_URL = 'http://localhost:3003/api/v1'
 
 // 登录页
 const LOGIN_PAGE = '/pages/login/index'
