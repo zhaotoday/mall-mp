2661d17f9dfd431884c0e34aa841099a9dfa71e8
zhaojintian <729234283@qq.com>
2019-05-19 19:49:06 +0800

    优化

---
+++ components/cart-manager/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="c-cart-manager c1">
+    <div class="c-cart-manager__checked bgc11 fs28">
+      <c-checkbox
+        class="c-cart-manager__checkbox"
+        :checked="true">
+      </c-checkbox>
+      <div class="fs28">已选中（2）</div>
+      <div class="c-cart-manager__money fs32">￥23.68</div>
+    </div>
+    <div class="c-cart-manager__settle bgc5 fs32 u-tac">去结算</div>
+  </div>
+</template>
+
+<script>
+import CCheckbox from '../checkbox/index'
+
+export default {
+  name: 'c-cart-manager',
+  components: { CCheckbox },
+  props: {}
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles.scss">
+</style>
---
+++ pages/tab-bar/cart/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="p-cart">
+    <ul class="c-products is-editable bgc1 u-mt20">
+      <li
+        v-for="item in productsList.items"
+        :key="item.id"
+        class="c-products__item">
+        <c-checkbox
+          class="c-products__checkbox"
+          :checked="true">
+        </c-checkbox>
+        <img
+          class="c-products__image"
+          :src="$helpers.getImageById(item.pictures)" />
+        <div class="c-products__info">
+          <div class="c-products__name fs32">{{ item.name }}</div>
+          <div class="c-products__desc fs22">香甜可口 夏日首选</div>
+          <div class="c-products__tag fs20">100 元/包</div>
+          <div class="c-products__price c5 fs40">
+            <span class="fs20">￥</span>
+            {{ item.price }}
+          </div>
+        </div>
+        <div class="c-products__cart c-icon c-icon--add-bg"></div>
+      </li>
+    </ul>
+    <c-cart-manager></c-cart-manager>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import CCheckbox from '@/components/checkbox'
+import CCartManager from '../../../components/cart-manager/index'
+
+export default {
+  components: { CCartManager, CCheckbox },
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState({
+      productsList: state => state['public/products'].list
+    })
+  },
+  onLoad () {
+    this.getProductsList()
+  },
+  methods: {
+    getProductsList () {
+      return this.$store.dispatch('public/products/getList', {
+        query: {}
+      })
+    }
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles.scss">
+</style>
