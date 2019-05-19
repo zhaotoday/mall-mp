b000d33281d0f7d356fa39344cdc9e988152af40
zhaojintian <729234283@qq.com>
2019-05-19 21:18:10 +0800

    update

--- pages/tab-bar/my/index.vue
+++ pages/tab-bar/my/index.vue
@@ -1,116 +1,39 @@
 <template>
-  <div class="p-uc">
-    <div class="pb-wrap">
+  <div class="p-my">
+    <div class="b-info bgc4 c1">
       <img
-        class="pb-avatar"
-        :src="user.avatarUrl" />
-      <div class="pb-nickname c1 fs34">{{ user.nickName }}</div>
-      <div class="pb-cell c-cell">
-        <div class="c-cell__item fs28">
-          <div class="c-cell__icon c-icon c-icon--point"></div>
-          <div class="c-cell__label">积分</div>
-          <div class="c-cell__value">100</div>
-        </div>
-        <div class="c-cell__item fs28">
-          <div class="c-cell__icon c-icon c-icon--contact"></div>
-          <div class="c-cell__label">客服</div>
-          <div
-            class="c-cell__value"
-            data-text="13950442340"
-            @click="handleCopyTelephone">
-            13950442340(微信手机同号)
-          </div>
-          <!--
-          <div class="c-cell__addon-icon c-icon c-icon--call"></div>
-          -->
-        </div>
-        <div class="c-cell__item fs28">
-          <div class="c-cell__icon c-icon c-icon--phone"></div>
-          <div class="c-cell__label">手机号</div>
-          <div class="c-cell__value">13950442340</div>
-        </div>
+        class="c-avatar"
+        src="./images/avatar.jpg" />
+      <div class="b-info__name fs40">赵丽颖</div>
+      <div class="b-info__phone fs32">手机号：13950442340</div>
+      <div class="b-card bgc1">
+        <ul class="b-orders">
+          <li
+            v-for="item in [ 'all', 'to-pay', 'to-deliver', 'in-deliver', 'finish' ]"
+            :key="item"
+            :class="[ 'b-orders__item', `b-orders__item--${item}` ]">
+            <div class="b-orders__title c10 fs26 u-tac">全部</div>
+          </li>
+        </ul>
       </div>
-    </div>
-    <div class="pb-point-tabs c-tabs">
-      <div class="c-tabs__tabs fs32">
-        <div
-          v-for="(item, index) in pointTypes"
-          :key="item.value"
-          :class="[ 'c-tabs__tab', { 'is-active': cPointTabs.current === index } ]"
-          @click="cPointTabs.current = index">
-          {{ item.label }}
-        </div>
-      </div>
-      <swiper
-        class="c-swiper__swiper"
-        :current="cPointTabs.current"
-        @change="handlePointContentChange">
-        <swiper-item>
-          <div class="c-tabs__content">
-            <ul class="c-list">
-              <li class="c-list__item">
-                <div class="c-list__item-title c2 fs30">您推荐了 15 个好友，获得 100 积分</div>
-                <div class="c-list__item-addon c4 fs24">2018-12-20</div>
-              </li>
-              <li class="c-list__item">
-                <div class="c-list__item-title c2 fs30">您推荐了 15 个好友，获得 100 积分</div>
-                <div class="c-list__item-addon c4 fs24">2018-12-20</div>
-              </li>
-              <li class="c-list__item">
-                <div class="c-list__item-title c2 fs30">您推荐了 15 个好友，获得 100 积分</div>
-                <div class="c-list__item-addon c4 fs24">2018-12-20</div>
-              </li>
-              <li class="c-list__item">
-                <div class="c-list__item-title c2 fs30">您推荐了 15 个好友，获得 100 积分</div>
-                <div class="c-list__item-addon c4 fs24">2018-12-20</div>
-              </li>
-            </ul>
-          </div>
-        </swiper-item>
-        <swiper-item>
-          <div class="c-tabs__content">
-            <div class="pb-point-tabs__no-data">暂无分享积分</div>
-          </div>
-        </swiper-item>
-      </swiper>
-
-
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      cPointTabs: {
-        current: 0
-      }
-    }
+const ORDER_STATUSES = [
+  // 'all', 'to-pay', 'to-deliver', 'in-deliver', 'finish'
+  {
+    value: 'all',
+    label: '全部'
   },
-  computed: {
-    user () {
-      return this.$auth.get().user
-    },
-    pointTypes () {
-      return this.$consts.POINT_TYPES
-    }
-  },
-  methods: {
-    async handleCopyTelephone (e) {
-      /*
-      await this.$wx.setClipboardData({
-        data: e.currentTarget.dataset.text
-      })
-
-      // this.$wx.showToast({ title: '已复制微信号，请返回微信添加好友' })
-      */
-    },
-    handlePointContentChange (e) {
-      this.cPointTabs.current = e.mp.detail.current
-    }
+  {
+    value: 'to-pay',
+    label: '待付款'
   }
-}
+]
+
+export default {}
 </script>
 
 <style
