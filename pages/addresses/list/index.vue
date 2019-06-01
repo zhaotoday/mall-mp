c0a1165db6401883573413afe350e8d4caaa5a03
zhaojintian <729234283@qq.com>
2019-06-01 15:56:26 +0800

    update

--- pages/addresses/list/index.vue
+++ pages/addresses/list/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="p-addresses-list">
-
+    <ul class="b-list u-mt20">
+      <li class="b-list__item bgc1">
+        <div class="b-list__head">
+          <h2 class="b-list__title fs28">浦上大道金港路8号 金山桔园泰园</h2>
+          <p class="b-list__desc c9 fs24">赵金添 13890998888</p>
+          <div class="c-tag h34 bdc5 c5 fs20">公司</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
