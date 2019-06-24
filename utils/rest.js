5809358376df56967c5b97d0a7b71045df7ef2d9
zhaojintian <729234283@qq.com>
2019-06-25 07:56:21 +0800

    update

--- utils/rest.js
+++ utils/rest.js
@@ -9,59 +9,43 @@ export default class extends REST {
    * 重写父类 request 方法，按业务场景定制功能
    * @override
    */
-  request (method = 'GET', options = {}) {
-    if (!options.query) {
-      options.query = {}
-    }
-
-    if (!options.body) {
-      options.body = {}
-    }
-
-    if (typeof options.showError === 'undefined') {
-      options.showError = true
-    }
-
-    if (typeof options.loading === 'undefined') {
-      options.loading = true
-    }
-
+  request (method = 'GET', { query = {}, body = {}, showLoading = true, showError = true }) {
     if (auth.loggedIn()) {
       const userId = auth.get()['user']['id']
 
-      options.query.wxUserId = userId
-      options.body.wxUserId = userId
+      query.wxUserId = userId
+      body.wxUserId = userId
     }
 
     // 转 options.query.where 对象为字符串
-    if (options.query.where) {
-      options.query.where = restHelpers.whereToStr(options.query.where)
+    if (query.where) {
+      query.where = restHelpers.whereToStr(options.query.where)
     }
 
+    // 清楚缓存
     if (method === 'GET') {
-      options.query._ = new Date().getTime()
+      query._ = new Date().getTime()
     }
 
-    // options.loading && wxb.showLoading()
+    showLoading && wxb.showLoading()
 
     return new Promise(resolve => {
       super.request(method, options)
         .then(res => {
-          options.loading && wxb.hideLoading()
-
           // 在这里可对 res 进行包装
           resolve(res.data)
         })
         .catch(res => {
-          options.loading && wxb.hideLoading()
-
           if (res.statusCode === 500) {
             options.showError && wxb.showToast({ title: '服务器出错' })
           } else if (res.data.error.code === 'AUTHORIZATION/UNAUTHORIZED') {
             wxb.navigateTo({ url: consts.LOGIN_PAGE })
           } else {
-            options.showError && wxb.showToast({ title: res.data.error.message })
+            showError && wxb.showToast({ title: res.data.error.message })
           }
+        })
+        .finally(()=>{
+          showLoading && wxb.hideLoading()
         })
     })
   }
