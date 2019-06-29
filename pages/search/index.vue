f8f1799df469919f47fca0cf479c5ad8038af1d4
zhaojintian <729234283@qq.com>
2019-06-29 23:13:40 +0800

    update

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="p-search">
     <c-search has-submit></c-search>
+    <h2 class="b-title fs28">历史搜索</h2>
+    <ul class="b-tags">
+      <li class="b-tags__item c-tag h60 bgc7 c10 fs28">好又多</li>
+      <li class="b-tags__item c-tag h60 bgc7 c10 fs28">好又多</li>
+      <li class="b-tags__item c-tag h60 bgc7 c10 fs28">好又多</li>
+      <li class="b-tags__item c-tag h60 bgc7 c10 fs28">好又多</li>
+    </ul>
   </div>
 </template>
 
