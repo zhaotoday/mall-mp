a68715551b285a2e028a77a094a139d942a7648c
zhaojintian <729234283@qq.com>
2019-06-29 22:41:22 +0800

    update

--- components/search/index.vue
+++ components/search/index.vue
@@ -3,8 +3,8 @@
     <div class="c-search__input bgc1">
       <input
         class="fs28"
-        type="text"
-        placeholder="输入要搜索的商品名称" />
+        placeholder="输入要搜索的商品名称"
+      />
     </div>
     <div class="c-search__submit c1 fs28">搜索</div>
   </div>
