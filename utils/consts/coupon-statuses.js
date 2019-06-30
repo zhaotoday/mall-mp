6b82984777c5088d69b1227a3380d784a213bee7
zhaojintian <729234283@qq.com>
2019-06-30 21:20:44 +0800

    update

--- utils/consts/coupon-statuses.js
+++ utils/consts/coupon-statuses.js
@@ -1,1 +1,18 @@
-export default []
+export default [
+  {
+    value: '1',
+    label: '全部'
+  },
+  {
+    value: '2',
+    label: '未使用'
+  },
+  {
+    value: '3',
+    label: '已使用'
+  },
+  {
+    value: '4',
+    label: '已过期'
+  }
+]
