fdd8f1d3a68eeab9c7326f764223e2fe8012cb66
zhaojintian <729234283@qq.com>
2019-07-10 16:38:24 +0800

    update

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -9,69 +9,73 @@
         {{ item.label }}
       </li>
     </ul>
-    <ul class="b-list">
-      <li
-        v-for="order in list.items"
-        :key="order.id"
-        class="b-list__item bgc1 u-mb20">
-        <div class="b-list__head">
+    <scroll-view
+      class="b-list"
+      scroll-y>
+      <ul class="u-mt20">
+        <li
+          v-for="order in list.items"
+          :key="order.id"
+          class="b-list__item bgc1 u-mb20">
+          <div class="b-list__head">
           <span
             class="fs28"
             style="padding-right: 10rpx;">
             {{ $helpers.getItem($consts.ORDER_STATUSES, 'code', order.status)['label'] }}
           </span>
-          <span class="c8 fs22">{{ $time.getTime(order.createdAt) }}</span>
-        </div>
-        <div class="b-list__body o-media">
-          <img
-            class="o-media__image"
-            mode="aspectFill"
-            :src="$helpers.getImageURL({ id: order.products[0] ? order.products[0].pictures : 0, width: 110, height: 110 })"
-          />
-          <div class="o-media__body">
-            <div class="b-products">
-              <div
-                v-for="product in order.products"
-                :key="product.id">
-                <template v-if="product.price">
-                  <div class="b-products__item c10 fs26">
-                    {{ product.name }}
-                    <div class="b-products__number">x{{ product.number }}</div>
-                  </div>
-                </template>
-                <template v-else>
-                  <div
-                    v-for="specification in product.specifications"
-                    :key="specification.value"
-                    v-if="specification.number"
-                    class="b-products__item c10 fs26">
-                    {{ product.name }}（{{ specification.price }} 元 / {{ specification.label }}）
-                    <div class="b-products__number">x{{ specification.number }}</div>
-                  </div>
-                </template>
-              </div>
-            </div>
-            <div class="b-money fs28">共 {{ getTotalNumber(order.products) }} 份，实付 ￥{{ order.paidMoney }}</div>
+            <span class="c8 fs22">{{ $time.getTime(order.createdAt) }}</span>
           </div>
-        </div>
-        <div class="b-list__foot">
-          <template v-if="order.status === '1'">
-            <div
-              class="c-button w160 h48 bgc4 c1 fs24"
-              @click="pay(order)">
-              立即付款
+          <div class="b-list__body o-media">
+            <img
+              class="o-media__image"
+              mode="aspectFill"
+              :src="$helpers.getImageURL({ id: order.products[0] ? order.products[0].pictures : 0, width: 110, height: 110 })"
+            />
+            <div class="o-media__body">
+              <div class="b-products">
+                <div
+                  v-for="product in order.products"
+                  :key="product.id">
+                  <template v-if="product.price">
+                    <div class="b-products__item c10 fs26">
+                      {{ product.name }}
+                      <div class="b-products__number">x{{ product.number }}</div>
+                    </div>
+                  </template>
+                  <template v-else>
+                    <div
+                      v-for="specification in product.specifications"
+                      :key="specification.value"
+                      v-if="specification.number"
+                      class="b-products__item c10 fs26">
+                      {{ product.name }}（{{ specification.price }} 元 / {{ specification.label }}）
+                      <div class="b-products__number">x{{ specification.number }}</div>
+                    </div>
+                  </template>
+                </div>
+              </div>
+              <div class="b-money fs28">共 {{ getTotalNumber(order.products) }} 份，实付 ￥{{ order.paidMoney }}</div>
             </div>
-          </template>
-          <template v-else-if="order.status === '2'">
-            <div
-              class="c-button w160 h48 bgc1 bdc4 c4 fs24"
-              @click="clone(order)">
-              再来一单
-            </div>
-          </template>
-        </div>
-      </li>
-    </ul>
+          </div>
+          <div class="b-list__foot">
+            <template v-if="order.status === '1'">
+              <div
+                class="c-button w160 h48 bgc4 c1 fs24"
+                @click="pay(order)">
+                立即付款
+              </div>
+            </template>
+            <template v-else-if="order.status === '2'">
+              <div
+                class="c-button w160 h48 bgc1 bdc4 c4 fs24"
+                @click="clone(order)">
+                再来一单
+              </div>
+            </template>
+          </div>
+        </li>
+      </ul>
+    </scroll-view>
     <c-empty v-if="!list.items.length && loaded"></c-empty>
   </div>
 </template>
