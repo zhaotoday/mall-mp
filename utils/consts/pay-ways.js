c061992a6c151295add94d83990f4430831c1815
zhaojintian <729234283@qq.com>
2019-09-19 14:54:48 +0800

    update

--- utils/consts/pay-ways.js
+++ utils/consts/pay-ways.js
@@ -1,10 +1,10 @@
 export default [
   {
-    value: 'WECHATPAY',
-    label: '在线支付'
-  },
-  {
     value: 'CASH_ON_DELIVERY',
     label: '货到付款'
+  },
+  {
+    value: 'WECHATPAY',
+    label: '在线支付'
   }
 ]
