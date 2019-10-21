825627423b0497ee18b7e23b707ad48497cde78f
zhaojintian <729234283@qq.com>
2019-10-22 01:54:29 +0800

    update

--- pages/apply-merchant/index.vue
+++ pages/apply-merchant/index.vue
@@ -1,33 +1,62 @@
 <template>
   <div class="p-join c-comment">
-    <h2 class="c-comment__title fs30">商家名称</h2>
+    <h2 class="c-comment__title fs28">商家名称</h2>
     <input
       class="c-comment__input fs26 bgc1"
       placholder-class="c9"
       placeholder="请输入商家名称"
       v-model="cForm.name"
     />
-    <h2 class="c-comment__title fs30">联系人</h2>
+    <h2 class="c-comment__title fs28">店铺照片</h2>
+    <div
+      v-if="!cForm.picture"
+      class="c-comment__upload bgc1"
+      @click="chooseImage()">
+    </div>
+    <div
+      v-else
+      class="c-comment__upload-file bgc1">
+      <img :src="$helpers.getImageById(this.cForm.picture)" />
+      <i
+        class="c-icon c-icon--close"
+        @click="delImage">
+      </i>
+    </div>
+    <h2 class="c-comment__title fs28">联系人</h2>
     <input
       class="c-comment__input fs26 bgc1"
       placholder-class="c9"
       placeholder="请输入联系人"
       v-model="cForm.contactName"
     />
-    <h2 class="c-comment__title fs30">手机号</h2>
+    <h2 class="c-comment__title fs28">手机号</h2>
     <input
       class="c-comment__input fs26 bgc1"
       placholder-class="c9"
       placeholder="请输入手机号"
       v-model="cForm.phoneNumber"
     />
-    <h2 class="c-comment__title fs30">备注</h2>
-    <textarea
-      class="c-comment__textarea fs26 bgc1"
+    <h2 class="c-comment__title fs28">收货地址</h2>
+    <input
+      class="c-comment__input fs26 bgc1"
       placholder-class="c9"
-      placeholder="请输入备注"
-      v-model="cForm.remark">
-    </textarea>
+      placeholder="请输入收货地址"
+      v-model="cForm.address"
+    />
+    <h2 class="c-comment__title fs28">送货时间</h2>
+    <input
+      class="c-comment__input fs26 bgc1"
+      placholder-class="c9"
+      placeholder="请输入送货时间"
+      v-model="cForm.deliveryTime"
+    />
+    <h2 class="c-comment__title fs28">员工代码</h2>
+    <input
+      class="c-comment__input fs26 bgc1"
+      placholder-class="c9"
+      placeholder="请输入员工代码"
+      v-model="cForm.staffNo"
+    />
     <button
       class="c-button is-foot w670 h76 bgc4 c1 fs32"
       @click="submit">
--- pages/apply-merchant/script.js
+++ pages/apply-merchant/script.js
@@ -1,19 +1,48 @@
+import restHelpers from '@/utils/helpers/rest-helpers'
+
 export default {
   data () {
     return {
       cForm: {
         name: '',
+        picture: '',
         contactName: '',
-        remark: ''
+        phoneNumber: '',
+        address: '',
+        deliveryTime: '',
+        staffNo: ''
       }
     }
   },
   methods: {
+    async chooseImage () {
+      const { tempFilePaths } = await this.$wx.chooseImage({
+        count: 1,
+        sizeType: ['compressed']
+      })
+
+      const { data } = await this.$wx.uploadFile({
+        url: this.$consts.API_URL + '/wx/files',
+        header: restHelpers.getHeaders(),
+        filePath: tempFilePaths[0],
+        name: 'file'
+      })
+
+      this.cForm.picture = JSON.parse(data)['data'].id
+    },
+    delImage () {
+      this.cForm.picture = 0
+    },
     async submit () {
-      const { name, contactName, phoneNumber } = this.cForm
+      const { name, picture, contactName, phoneNumber, address, deliveryTime } = this.cForm
 
       if (!name) {
         this.$wx.showToast({ title: '请输入商家名称' })
+        return
+      }
+
+      if (!picture) {
+        this.$wx.showToast({ title: '请上传店铺照片' })
         return
       }
 
@@ -29,6 +58,16 @@ export default {
 
       if (!/^1\d{2}\s?\d{4}\s?\d{4}$/.test(phoneNumber)) {
         this.$wx.showToast({ title: '手机号格式错误' })
+        return
+      }
+
+      if (!address) {
+        this.$wx.showToast({ title: '请输入收货地址' })
+        return
+      }
+
+      if (!deliveryTime) {
+        this.$wx.showToast({ title: '请输入送货时间' })
         return
       }
 
